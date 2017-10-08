121fa599805ce1c2e53a74d378c2744131802817
Christian Liebel <christian.liebel@thinktecture.com>
2017-10-08 18:22:01 +0200

    ex0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: 'home',
+      component: HomeComponent
+    }, {
+      path: 'info',
+      component: InfoComponent
+    }, {
+      path: '',
+      pathMatch: 'full',
+      redirectTo: 'home'
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
